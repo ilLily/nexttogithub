fb4f38e8268b9841df8ca0d0f896352e7def90c0
Lily Kuo <ilwitulily0209@gmail.com>
2023-07-18 11:00:04 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  basePath: '/next-temple',
+  assetPrefix: '/next-temple',
 }
-
-module.exports = nextConfig
+module.exports = nextConfig;
